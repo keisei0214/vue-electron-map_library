536f649f65e2d9e32ce89e6d38971e19267100e2
DESKTOP-M0OOP2T\Keisuke Isemoto <5ek2u0i1ek9@gmail.com>
2019-10-03 12:29:16 +0900

    menuの追加

--- src/renderer/page/Main.vue
+++ src/renderer/page/Main.vue
@@ -16,6 +16,19 @@ export default {
   methods: {
     setMenu () {
       const menu = new remote.Menu()
+      // fileメニューの作成
+      menu.append(
+        new remote.MenuItem({
+          label: 'File',
+          submenu: [
+            {
+              role: process.platform === 'darwin' ? 'close' : 'quit'
+            }
+          ]
+        })
+      )
+
+      // map切り替えメニューの作成
       const submenuList = []
       Object.keys(this.$refs.map.$data.tile).forEach((e) => {
         submenuList.push(
